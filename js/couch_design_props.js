4d1f983b785854ae846f0436df59e3a43b31a260
Eric Robitaille <eric@vianet.ca>
2012-02-18 10:41:54 -0500

    added couchdb design view/properties to get tabs with tags

---
+++ js/couch_design_props.js
@@ -0,0 +1,16 @@
+exports.views = {
+    get_tabs: {
+        map: function (doc) {
+					if(doc.type)
+					{
+						if (doc.type == 'doc')
+						{
+							emit(doc.tab, doc.tags);
+						}
+					}
+        },
+				reduce: function(keys, values) {
+					return values.join(',').split(',');
+				}
+    }
+};
